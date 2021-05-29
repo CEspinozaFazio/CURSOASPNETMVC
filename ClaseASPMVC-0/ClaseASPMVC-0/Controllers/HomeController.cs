adc076131edf7fbbd6c06cb01bda0ecc4819ebc1
Carlos Espinoza <37185995+CEspinozaFazio@users.noreply.github.com>
2021-05-28 19:58:57 -0500

    Initial commit

---
+++ ClaseASPMVC-0/ClaseASPMVC-0/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClaseASPMVC_0.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Menu1()
+        {
+            ViewBag.Message = "Menú 1";
+
+            return View();
+        }
+        public ActionResult Menu2()
+        {
+            ViewBag.Message = "Menú 2";
+
+            return View();
+        }
+    }
+}
